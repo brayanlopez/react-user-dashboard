a34a85cc1ebdb26942c3c1686adb8ae1b4dcbdd3
Brayan Lopez <brayan.lopez@sophossolutions.com>
2022-05-19 10:45:06 -0500

    [FIXED] value of isLogin in appSlice

--- src/redux-slices/index.js
+++ src/redux-slices/index.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 export const appSlice = createSlice({
   name: "app",
   initialState: {
-    isLogin: true,
+    isLogin: false,
   },
   reducers: {
     login: (state) => {
